db947b77b874b3ea2b8a4ff3c237f5511b86815a
Charbel <charbelelbateh@gmail.com>
2022-08-08 14:07:42 +0300

    Overall Improvment

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -1,33 +1,105 @@
-const grid = document.querySelector('grid')
-const colorPicker = document.getElementById('colorPicker')
-const width = grid.offsetWidth;
-var color = "#000000"
-let size = 20;
+/***********Project Etch-A-Sketch ***********/
 
-for (let i = 0; i < size * size; i++) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    const gridElement = document.createElement('div')
-    gridElement.classList.add('grid-element')
-    grid.appendChild(gridElement)
+//Constants and Default Values
+const grid = document.querySelector('grid');
+const slider = document.querySelector('input.slider');
+const colorPicker = document.getElementById('colorPicker');
+const buttons = document.querySelectorAll('button');
+const clearButton = document.querySelector('button.clear');
+const colorButton = document.querySelector('button.color');
+const rainbowButton = document.querySelector('button.rainbow');
+const eraser = document.querySelector('button.eraser');
+let currentMode = 'COLOR';
+let mouseDown = false;
+var color = "#000000";
+let size = 10;
+
+//Loading Phase
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+window.onload = () => {
+    colorButton.classList.add('active');
+    setupGrid(size);
 }
 
-const setColor = (input)=>{
-    color = input;
+//Grid setup
+function setupGrid(size){
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mousedown', changeColor);
+        gridElement.addEventListener('mouseover', changeColor);
+        grid.appendChild(gridElement);
+    }
 }
+const gridElement = document.querySelectorAll('div.gridElement');
 
+//Event Listeners
 colorPicker.addEventListener("input", ()=>{
-    setColor(colorPicker.value)
+    color = colorPicker.value;
 })
 
-const gridElement = document.querySelectorAll('div');
-
-gridElement.forEach(element => {
-    element.addEventListener('mousedown', () => {
-        gridElement.forEach(box => {
-            box.addEventListener('mouseover', ()=>{
-                    box.style.backgroundColor = color;
-            },false)
-        })
-    })
+slider.addEventListener('input', ()=>{
+    clearGrid();
+    setupGrid(slider.value);
 })
+
+clearButton.addEventListener('click', ()=>{
+    clearGrid();
+})
+
+colorButton.addEventListener('click', ()=>{
+    clearMode();
+    colorButton.classList.add('active');
+    currentMode = 'COLOR';
+})
+
+rainbowButton.addEventListener('click', ()=>{
+    clearMode();
+    rainbowButton.classList.add('active');
+    currentMode = 'RAINBOW';
+})
+
+eraser.addEventListener('click', ()=>{
+    clearMode();
+    eraser.classList.add('active');
+    currentMode = 'ERASER';
+})
+
+// Color changes for the grid
+function changeColor(e){
+    if(e.type === 'mouseover' && !mouseDown){
+        return;
+    }else{
+        switch (currentMode) {
+            case 'COLOR':
+                e.target.style.backgroundColor = color;
+                break;
+            case 'ERASER':
+                e.target.style.backgroundColor = '#ffffff';
+                break;
+            case 'RAINBOW':
+                const R = Math.floor(Math.random() * 256)
+                const G = Math.floor(Math.random() * 256)
+                const B = Math.floor(Math.random() * 256)
+                e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+//Basic Functions
+function clearGrid(){
+    grid.innerHTML='';
+    setupGrid(slider.value);
+}
+
+function clearMode(){
+    buttons.forEach(button => {
+        button.classList.remove('active');
+    });
+}
